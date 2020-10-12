0fa787cabb9daf5548adac5c989008b7eeafa2b1
berellevy <berellevy@gmail.com>
2020-10-12 17:08:28 -0400

    up to submitting answer

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -12,12 +12,19 @@
         <b-list-group-item
           v-for="(answer, index) in answers"
           :key="index"
+          @click="selectedAnswer(index)"
+          :class="[selectedIndex === index ? 'selected' : '']"
         >
           {{ answer }}
         </b-list-group-item>
       </b-list-group>
 
-      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button 
+        variant="primary"
+        @click="submitAnswer"
+      >
+        Submit
+      </b-button>
       <b-button @click="next" variant="success" href="#">
         Next
       </b-button>
@@ -26,18 +33,66 @@
 </template>
 
 <script>
+  import _ from 'lodash'
   export default {
     props: {
       currentQuestion: Object,
       next: Function,
+    },
+    data() {
+      return {
+        selectedIndex: null,
+        shuffledAnswers: [],
+      }
     },
     computed: {
       answers() {
         return [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer]
       },
     },
-    mounted() {
-      console.log(this.currentQuestion)
+    watch: {
+      currentQuestion: {
+        immediate: true,
+        handler() {
+          this.selectedIndex = null
+          this.shuffleAnswers()
+        }
+      },
+
+    },
+    methods: {
+      selectedAnswer(index) {
+        this.selectedIndex = index;
+      },
+      shuffleAnswers() {
+        let answers = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer]
+        this.shuffledAnswers = _.shuffle(answers)
+      }
     },
   }
 </script>
+
+<style scoped>
+  .list-group {
+    margin-bottom: 15px;
+  }
+  .list-group-item:hover {
+    background: #EEE;
+    cursor: pointer;
+  }
+  .btn {
+    margin: 0 5px;
+  }
+
+  .selected {
+    background-color: lightblue;
+  }
+
+  .correct {
+    background-color: lightgreen;
+  }
+
+  .incorrect {
+    background-color: red;
+  }
+</style>
